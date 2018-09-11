89c122e64193cdc6c073b1773c5f9d9f3b1bd188
Willem Delventhal <will.delcenthal@gmail.com>
2018-09-11 11:30:40 -0700

    Pachinko added

---
+++ Fall - 2018/Actuals/Pachinko1/Assets/PrefabSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour 
+{
+    public GameObject prefabToSpawn;
+
+    public void SpawnPrefab()
+    {
+        GameObject spawnedPrefab = Instantiate( prefabToSpawn ) as GameObject;
+
+        Vector3 spawnPosition = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+
+        spawnPosition.z = spawnedPrefab.transform.position.z;
+        spawnedPrefab.transform.position = spawnPosition;
+    }
+}
---
+++ Fall - 2018/Actuals/Pachinko1/Assets/Scripts/PointAdder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAdder : MonoBehaviour
+{
+    public int pointsToAdd;
+
+    public void OnTriggerEnter2D( Collider2D collision )
+    {
+        Debug.Log( "You scored " + pointsToAdd + " points!" );
+    }
+}
